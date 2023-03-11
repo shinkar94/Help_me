12c5d01b00b90555d510c5f76dc660c46af214e6
shinkar94 <romka94shinkarenko@gmail.com>
2023-03-12 00:05:14 +0300

    update icon

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {state} from "./MyState/MyState";
 // import {state} from "./MyState/State";
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App state={state}/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
